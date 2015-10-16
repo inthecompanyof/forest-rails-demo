42f80edf15d8a92d5af40043f1293d85968460ba
Sandro Munda <sandro@munda.me>
2015-10-16 16:54:50 +0200

    Add the Log table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015103440) do
+ActiveRecord::Schema.define(version: 20151015174346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 20151015103440) do
   end
 
   add_index "items", ["supplier_id"], name: "index_items_on_supplier_id", using: :btree
+
+  create_table "logs", force: :cascade do |t|
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "suppliers", force: :cascade do |t|
     t.string   "first_name"
