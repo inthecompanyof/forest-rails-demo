2d5fc3fd736b317593179e9b5fe142470ac4aa8c
Sandro Munda <sandro@munda.me>
2015-10-21 16:33:50 +0200

    Add a default url picture

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ActiveRecord::Base
   has_and_belongs_to_many :customers
   belongs_to :supplier
 
-  has_attached_file :picture, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :picture, styles: {
+     medium: '300x300>',
+     thumb: '100x100>'
+  }, default_url: 'http://lorempixel.com/300/200/'
+
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
