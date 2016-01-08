7206e998fcb2c46e94a567acd4b1309a7fcd88f1
Sandro Munda <sandro@munda.me>
2016-01-08 15:16:19 +0100

    WIP

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,13 +1,13 @@
 ForestLiana.jwt_signing_key = ENV['FOREST_KEY']
 ForestLiana.integrations = {
-  #stripe: {
-    #api_key: ENV['STRIPE_SECRET_KEY'],
-    #user_collection: 'Customer',
-    #user_field: 'stripe_id'
-  #},
-  #intercom: {
-    #app_id: ENV['INTERCOM_APP_ID'],
-    #api_key: ENV['INTERCOM_API_KEY'],
-    #user_collection: 'Customer'
-  #}
+  stripe: {
+    api_key: ENV['STRIPE_SECRET_KEY'],
+    user_collection: 'Customer',
+    user_field: 'stripe_id'
+  },
+  intercom: {
+    app_id: ENV['INTERCOM_APP_ID'],
+    api_key: ENV['INTERCOM_API_KEY'],
+    user_collection: 'Customer'
+  }
 }
